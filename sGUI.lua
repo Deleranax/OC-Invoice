abdd4500d58559ce30f0d5bd1697565533e6180d
Deleranax <reazer18@gmail.com>
2020-07-27 01:58:02 +0200

    Update sGUI.lua

--- sGUI.lua
+++ sGUI.lua
@@ -66,11 +66,12 @@ function sgui.addButton(x, y, w, h, txt, func, fcolor, bcolor)
     end
     
     function button.update()
-    while true do
-        local evs = {term.pull("touch")}
-        if evs[3] >= button.properties[1] and evs[3] <= button.properties[1] + button.properties[3] and evs[4] >= button.properties[2] and evs[3] <= button.properties[2] + button.properties[3] then
-            button.properties[6]()
-            break
+        while true do
+            local evs = {term.pull("touch")}
+            if evs[3] >= button.properties[1] and evs[3] <= button.properties[1] + button.properties[3] and evs[4] >= button.properties[2] and evs[3] <= button.properties[2] + button.properties[3] then
+                button.properties[6]()
+                break
+            end
         end
     end
     
